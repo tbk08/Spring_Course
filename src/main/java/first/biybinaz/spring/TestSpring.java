57d9a679988404ef090a6ca90febfd84877da76d
tbk <turemuratova08@gmail.com>
2023-02-10 12:10:25 +0500

    DI внедрение двойной зависимости

--- src/main/java/first/biybinaz/spring/TestSpring.java
+++ src/main/java/first/biybinaz/spring/TestSpring.java
@@ -7,8 +7,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
